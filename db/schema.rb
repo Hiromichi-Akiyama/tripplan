bd5bdb84be9a4b6267f4f34794113f8b02086e53
Hiromichi-Akiyama <fox37450@gmail.com>
2025-12-17 16:15:21 +0900

    feat: add trips table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_16_085040) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_17_012900) do
+  create_table "trips", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.string "destination"
+    t.date "start_date", null: false
+    t.date "end_date", null: false
+    t.string "color"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_trips_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", limit: 50
     t.string "email", default: "", null: false
@@ -24,4 +37,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_16_085040) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "trips", "users"
 end
