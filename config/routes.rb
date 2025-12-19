1f481dacf7e5910dee64341d1ac8ee74445bc8fd
Hiromichi-Akiyama <fox37450@gmail.com>
2025-12-19 22:34:07 +0900

    TripsのCRUDルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :trips, only: [:index, :show]
+  resources :trips, only: %i[index show new create edit update destroy]
   # 将来的にonlyは外す予定
 end
