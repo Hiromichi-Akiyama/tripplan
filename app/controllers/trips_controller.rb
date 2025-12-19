1552f0b206ea788a885a7c0ebd826c3968ae1a57
Hiromichi-Akiyama <fox37450@gmail.com>
2025-12-19 22:36:13 +0900

    Tripsの新規作成・更新・削除処理を追加（成功/失敗分岐含む）

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,10 +1,52 @@
 class TripsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_trip, only: %i[show edit update destroy]
+
   def index
     @active_tab = Trip::TABS.include?(params[:tab]) ? params[:tab] : "all"
     @trips = current_user.trips.for_tab(@active_tab)
   end
 
   def show
+  end
+
+  def new
+    @trip = current_user.trips.build
+  end
+
+  def create
+    @trip = current_user.trips.build(trip_params)
+
+    if @trip.save
+      redirect_to trip_path(@trip), notice: "旅行を作成しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @trip.update(trip_params)
+      redirect_to trip_path(@trip), notice: "旅行を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @trip.destroy
+    redirect_to trips_path, notice: "旅行を削除しました"
+  end
+
+  private
+
+  def set_trip
     @trip = current_user.trips.find(params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(:title, :destination, :start_date, :end_date, :color, :notes)
   end
 end
