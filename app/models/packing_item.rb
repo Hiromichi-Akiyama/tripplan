774f8c91bb3e2cc38c8bad0cc74f87678a2af951
Hiromichi-Akiyama <fox37450@gmail.com>
2025-12-26 13:20:31 +0900

    feat: PackingItemのバリデーション/並び順scope追加 + Tripにpacking_items関連付け追加

--- app/models/packing_item.rb
+++ app/models/packing_item.rb
@@ -1,2 +1,17 @@
 class PackingItem < ApplicationRecord
+  belongs_to :trip
+
+  CATEGORIES = ["衣類", "洗面・バス用品", "電子機器", "貴重品・書類", "薬・ヘルスケア", "その他"].freeze
+
+  validates :name, presence: true
+  validates :display_order, numericality: { only_integer: true, greater_than_or_equal_to: 1}
+  validates :category, inclusion: {in: CATEGORIES}, allow_blank: true
+
+  scope :ordered_for_list, -> {
+    order(category: :asc)
+      .order(checked: :asc)
+      .order(display_order: :asc)
+      .order(id: :asc)
+  }
+
 end
