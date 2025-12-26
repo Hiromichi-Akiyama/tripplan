59142afef8fbb2819c55b56ab52f6ad6de527d5c
Hiromichi-Akiyama <fox37450@gmail.com>
2025-12-26 17:22:56 +0900

    feat: PackingItemsをTripにネストしたルーティングを追加（new/create/edit/update/destroy）

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   resources :trips, only: %i[index show new create edit update destroy] do
     resources :activities, only: %i[new create edit update destroy] # index/showは不要
+    resources :packing_items, only: %i[new create edit update destroy]
   end
 end
