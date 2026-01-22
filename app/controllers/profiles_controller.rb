f55ddd8e5268af0d1902373a2237bba51408de46
Hiromichi-Akiyama <fox37450@gmail.com>
2026-01-22 12:18:02 +0900

    refactor: プロフィール更新ロジックをUserモデルへ移し ProfilesController を簡素化

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,7 +7,7 @@ class ProfilesController < ApplicationController
 
   def update
     @user = current_user
-    if update_profile(@user, profile_params)
+    if @user.update_profile(profile_params)
       redirect_to edit_profile_path, notice: "プロフィールを更新しました"
     else
       flash.now[:alert] = "入力内容にエラーがあります。確認してください。"
@@ -19,13 +19,5 @@ class ProfilesController < ApplicationController
 
   def profile_params
     params.require(:user).permit(:name, :email)
-  end
-
-  def update_profile(user, attrs)
-    if user.respond_to?(:update_without_password)
-      user.update_without_password(attrs)
-    else
-      user.update(attrs)
-    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,17 @@ class User < ApplicationRecord
     demo
   end
 
+  # Profile update policy
+  # Prefer Devise's update_without_password when available (e.g., updating name/email from a profile screen)
+  # so users are not forced to re-enter their password.
+  def update_profile(attrs)
+    if respond_to?(:update_without_password)
+      update_without_password(attrs)
+    else
+      update(attrs)
+    end
+  end
+
   scope :expired_demos, -> {
     where(demo: true)
       .where("demo_expires_at IS NOT NULL AND demo_expires_at < ?", Time.current)
