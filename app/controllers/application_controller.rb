cae056c0345aa5c57df6efbef487db657d0bb8ae
Hiromichi-Akiyama <fox37450@gmail.com>
2026-01-27 22:24:35 +0900

    chore: デモ機能を削除し、関連コード/README記述を整理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::Base
-  helper_method :demo_user?
-
-  def demo_user?
-    session[:demo_user_id].present? && current_user&.id == session[:demo_user_id].to_i
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: "users/sessions" }
+  devise_for :users
   root "pages#home"
-  get "/demo", to: "demo_sessions#create", as: :demo_login
-  delete "/demo", to: "demo_sessions#destroy", as: :demo_logout
   resource :profile, only: %i[edit update]
 
   resources :trips, only: %i[index show new create edit update destroy] do
