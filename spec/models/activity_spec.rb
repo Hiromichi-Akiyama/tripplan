e9242fa82a29820fd09486b8b25fce49176f1734
Hiromichi-Akiyama <fox37450@gmail.com>
2026-02-02 22:38:43 +0900

    test: モデルspecのエラーメッセージ期待値をi18nに合わせる

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -67,19 +67,19 @@ RSpec.describe Activity, type: :model do
       it 'costがマイナスだと登録できない' do
         @activity.cost = -1
         @activity.valid?
-        expect(@activity.errors.full_messages).to include("費用は0以上の整数を入力してください")
+        expect(@activity.errors.full_messages).to include("費用は0以上の値にしてください")
       end
 
       it 'costが小数だと登録できない' do
         @activity.cost = 1.5
         @activity.valid?
-        expect(@activity.errors.full_messages).to include("費用は0以上の整数を入力してください")
+        expect(@activity.errors.full_messages).to include("費用は整数で入力してください")
       end
 
       it 'costが文字列だと登録できない' do
         @activity.cost = "abc"
         @activity.valid?
-        expect(@activity.errors.full_messages).to include("費用は0以上の整数を入力してください")
+        expect(@activity.errors.full_messages).to include("費用は数値で入力してください")
       end
 
     end
