be2530ae5f98971549834d6e19aaa78da0eeefd6
Hiromichi-Akiyama <fox37450@gmail.com>
2026-02-03 08:43:20 +0900

    test: 自分のTrip詳細にアクセスできる正常系を追加

--- spec/requests/trips_auth_spec.rb
+++ spec/requests/trips_auth_spec.rb
@@ -3,6 +3,19 @@ require "rails_helper"
 RSpec.describe "Trips auth", type: :request do
   fixtures :users, :trips
 
+  describe "自分のTripsにはアクセスできる" do
+    it "GET /trips/:id は自分のTripにアクセスできる" do
+      owner = users(:owner)
+      trip = trips(:owner_trip)
+
+      sign_in owner
+      get trip_path(trip)
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include(trip.title)
+    end
+  end
+
   describe "未ログイン時のアクセス制御" do
     it "GET /trips はログイン画面へリダイレクトされる" do
       get trips_path
